f95e052ba84acbee512dd43fc5f00ea8ec1dbdb8
Miroslav Šerý <469343@muni.cz>
2023-03-28 18:53:23 +0200

    create tests with ids

--- SeleniumTests/SeleniumTestsExample.cs
+++ SeleniumTests/SeleniumTestsExample.cs
@@ -11,8 +11,7 @@ namespace SeleniumTests
         [SetUp]
         public void startBrowser()
         {
-            // TODO: Place here path to you chrome driver
-            driver = new ChromeDriver("");
+            driver = new ChromeDriver("C:\\Users\\athes\\Desktop\\chromedriver");
         }
 
         [Test]
@@ -21,6 +20,37 @@ namespace SeleniumTests
             driver.Url = "https://www.saucedemo.com";
 
             // TODO: Place your test code here
+
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack")).Click();
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-bike-light")).Click();
+
+            driver.FindElement(By.Id("shopping_cart_container")).Click();
+
+            Assert.AreEqual("Sauce Labs Backpack",driver.FindElement(By.Id("item_4_title_link")).Text);
+            Assert.AreEqual("Sauce Labs Bike Light", driver.FindElement(By.Id("item_0_title_link")).Text);
+
+            driver.FindElement(By.Id("checkout")).Click();
+
+            driver.FindElement(By.Id("first-name")).SendKeys("John");
+            driver.FindElement(By.Id("last-name")).SendKeys("Doe");
+            driver.FindElement(By.Id("postal-code")).SendKeys("11222");
+
+            driver.FindElement(By.Id("continue")).Click();
+
+            string summary_info = driver.FindElement(By.ClassName("summary_info")).Text;
+
+            Assert.True(summary_info.Contains("Free Pony Express Delivery!"));
+
+            // compute the total price
+            Assert.True(summary_info.Contains("$43.18"));
+
+            driver.FindElement(By.Id("finish")).Click();
+
+            Assert.True(driver.FindElement(By.Id("checkout_complete_container")).Text.Contains("Thank you for your order!"));
         }
 
         [TearDown]
